0ee96c08bbe892c51b6223f260095ae4565e5273
กีตาร์เอาเอง <guitar@odds.team>
2022-07-27 00:30:50 +0700

    set up emulater

--- Backend/condb.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con= mysqli_connect("localhost","root","454531","dbjoke_sam_kai") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
-  date_default_timezone_set('Asia/Bangkok');
-?>
